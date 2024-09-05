5a3a9b1ac7cb5cc7bf2af5176b2fa6f437038814
Andrei Kapralov <andreikapralov20@gmail.com>
2024-09-05 12:22:01 +0300

    create client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const getInformation = async() => {
+    try{
+      const data = await axios.post('https://api.skilla.ru/mango/getList',{
+        "date_start": "2024-01-01",
+        "date_end": "2024-01-31",
+        "in_out": "in"
+    },{
+      headers: {
+        'Authorization': 'testtoken'
+      }
+      });
+      console.log(data)
+    } catch{
+      console.log('error')
+    }
+  }
+
+  
+
+  useEffect(() => {
+    getInformation()
+  }, [])
 
   return (
     <>
